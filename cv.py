8bacaa61ea3dae52b1a11719acdbb10ad8ccddcc
yx_chai@whu.edu.cn <yx_chai@whu.edu.cn>
2017-07-31 11:29:47 +0800

    Few adjustments and use pickle file to store cv objects

--- cv.py
+++ cv.py
@@ -3,6 +3,8 @@ from pandas import DataFrame
 from dir_walker import walker
 import re
 import os
+import pickle
+
 
 class CV:
     def __init__(self, info, data, low, high, segments):
@@ -11,30 +13,32 @@ class CV:
         self.low = low
         self.high = high
         self.segments = segments
-        self.full_info = self.info+' '+str(self.low)+' to '+str(self.high)+' seg'+str(self.segments)
-    
-    def plot(self, *, labels = None, title = None, xlim = None, pos = plt.subplots(1,1)[1]):
+
+    def plot(self, *, labels=None, title=None, xlim=None, pos=plt.subplots(1, 1)[1]):
         if xlim is None:
             xlim = [self.low, self.high]
         if title is None:
             title = self.info
-        pos = self.data.plot(x = 'Potential',
-                             y = 'Current',
-                             xlim = xlim,
-                             ax = pos,
-                             label = labels,
-                             title = title,
-                             grid = True)
+        pos = self.data.plot(x='Potential',
+                             y='Current',
+                             xlim=xlim,
+                             ax=pos,
+                             label=labels,
+                             title=title,
+                             grid=True)
         pos.set_xlabel('Potential')
         pos.set_ylabel('Current')
-        
-    def save(self, filename, path = os.getcwd()):
+
+    def save(self, filename, path=os.getcwd()):
         header = ['Potential', 'Current']
-        self.data.to_csv(path+'/'+filename+'.csv',
-                         index = False,
-                         header = False,
-                         columns = header)
-        print("Saved to "+path+filename+'.csv')
+        self.data.to_csv(path + '/' + filename + '.csv',
+                         index=False,
+                         header=False,
+                         columns=header)
+        print("Saved to " + path + filename + '.csv')
+
+    def __str__(self):
+        return self.info + ' ' + str(self.low) + ' to ' + str(self.high) + ' seg' + str(self.segments)
 
 
 def find_segments(data):
@@ -45,21 +49,22 @@ def find_segments(data):
         seg_label.append(num_seg)
         if i + 2 == len(data) - 1:
             break
-        delta_1 = data['Potential'][i+1] - p
-        delta_2 = data['Potential'][i+2] - data['Potential'][i+1]
+        delta_1 = data['Potential'][i + 1] - p
+        delta_2 = data['Potential'][i + 2] - data['Potential'][i + 1]
         if delta_1 * delta_2 < 0:
             num_seg += 1
     seg_label.append(num_seg)
     # data_seg = DataFrame(data, index = [seg_label, list(range(len(data)))])
     return [num_seg, seg_label]
-    
-def txt_to_CV(file, info = 'CV test'):
+
+
+def txt_to_CV(file, info='CV test'):
     h = 0
     l = 0
     record = 0
     a = []
     with open(file, 'rt') as f:
-        print(file+' Opened')
+        print(file + ' Opened')
         for line in f:
             if not (h and l):
                 if line.startswith('High'):
@@ -83,11 +88,13 @@ def txt_to_CV(file, info = 'CV test'):
     [num_seg, seg_label] = find_segments(frame)
     frame.index = seg_label
     return CV(info, frame, low, high, list(range(1, num_seg + 1)))
-    
+
+
 def cycle_split(cv):
     '''
     Used to extract single cycle from CV curve to form a list
     '''
+
     def seg_to_cyc(num_seg):
         if num_seg % 2:
             cycle_all = [[1]]
@@ -98,13 +105,15 @@ def cycle_split(cv):
             for i in list(range(1, num_seg + 1, 2)):
                 cycle_all.append([i, i + 1])
         return cycle_all
+
     cv_cycles = []
     cycles = seg_to_cyc(len(cv.segments))
     for i in cycles:
         cv_cycles.append(CV(cv.info, cv.data.ix[i], cv.low, cv.high, i))
     return cv_cycles
 
-def overlap(cv_list, *, filename = 'cv_overlap', title = 'No title', pos = plt.subplots(1,1)[1], labels = None):
+
+def overlap(cv_list, *, filename='cv_overlap', title='No title', pos=plt.subplots(1, 1)[1], labels=None):
     '''
     Overlap CV_curve in given list
     '''
@@ -113,7 +122,7 @@ def overlap(cv_list, *, filename = 'cv_overlap', title = 'No title', pos = plt.s
     if labels is None:
         labels = []
         for cv in cv_list:
-            labels.append(cv.full_info)
+            labels.append(str(cv))
     for cv in cv_list:
         l.append(cv.low)
         h.append(cv.high)
@@ -121,32 +130,42 @@ def overlap(cv_list, *, filename = 'cv_overlap', title = 'No title', pos = plt.s
     high = max(h)
     for i, cv in enumerate(cv_list):
         # pos = cv.data.plot(x = 'Potential',
-                           # y = 'Current',
-                           # xlim = [low, high],
-                           # ax = pos,
-                           # label = labels,
-                           # title = title,
-                           # grid = True)
-        cv.plot(labels = labels[i], title = title, xlim = [low, high], pos = pos)
+        # y = 'Current',
+        # xlim = [low, high],
+        # ax = pos,
+        # label = labels,
+        # title = title,
+        # grid = True)
+        cv.plot(labels=labels[i], title=title, xlim=[low, high], pos=pos)
     pos.set_xlabel('Potential')
     pos.set_ylabel('Current')
-    plt.savefig(filename+'.png', dpi=300)
+    plt.savefig(filename + '.png', dpi=300)
 
-    
+
 def cv_collect(rootdir):
     cv_list = []
     for filename, subdir in walker(rootdir, re.compile('cv(.*?)txt')):
-        cv_list.append(txt_to_CV(subdir+'/'+filename, filename[:len(filename) - 4]))
-    for i, cv in enumerate(cv_list):
-        print(i, cv.info)
+        cv_list.append(txt_to_CV(subdir + '/' + filename, filename[:len(filename) - 4]))
+    with open(rootdir+'cv_list.txt', 'w') as f:
+        for i, cv in enumerate(cv_list):
+            f.write(str(i)+' '+cv.info)
+            f.write('\n')
+    print()
+    print('List of CV has been saved to '+rootdir+'cv_list.txt')
+    with open(rootdir+'cv_data.pickle', 'wb') as f:
+        pickle.dump(cv_list, f, pickle.HIGHEST_PROTOCOL)
+    print()
+    print("CV information has been dumped to "+rootdir+'cv_data.pickle')
     return cv_list
-    
+
+__all__ = ['CV', 'find_segments', 'cycle_split', 'txt_to_CV', 'cv_collect', 'overlap']
+
 if __name__ == '__main__':
     rootdir = 'T:/programs/python/py_CV/data/'
     for filename, subdir in walker(rootdir, re.compile('cv(.*?)txt')):
-        fig, pos = plt.subplots(1,1)
-        cv = txt_to_CV(subdir+'/'+filename, filename[:len(filename) - 4])
+        fig, pos = plt.subplots(1, 1)
+        cv = txt_to_CV(subdir + '/' + filename, filename[:len(filename) - 4])
         for i, cv_cycle in enumerate(cycle_split(cv)):
-            cv_cycle.plot(labels = 'cycle '+str(i + 1), pos = pos)
-        plt.savefig(subdir+'/'+cv.full_info+'.png', dpi=300)
-        print(subdir+'/'+cv.full_info+'.png'+' Saved')
+            cv_cycle.plot(labels='cycle ' + str(i + 1), pos=pos)
+        plt.savefig(subdir + '/' + str(cv) + '.png', dpi=300)
+        print(subdir + '/' + str(cv) + '.png' + ' Saved')
--- overlap.py
+++ overlap.py
@@ -1,25 +1,60 @@
 from cv import *
 rootdir = 'T:/programs/python/py_CV/data/'
 cv_list = cv_collect(rootdir)
+import os.path
+import pickle
+
+rootdir = 'T:/programs/python/py_CV/test/'
+
+if os.path.isfile(rootdir+'cv_data.pickle'):
+    with open(rootdir+'cv_data.pickle', 'rb') as f:
+        cv_list = pickle.load(f)
+else:
+    cv_list = cv_collect(rootdir)
+
+# pos = plt.subplots(1,1)[1]
+# cv_overlap = []
+# cv_overlap.append(cycle_split(cv_list[4])[2])
+# cv_overlap.append(cycle_split(cv_list[11])[2])
+# cv_overlap.append(cycle_split(cv_list[18])[2])
+# cv_overlap.append(cycle_split(cv_list[23])[8])
+# cv_overlap.append(cycle_split(cv_list[33])[2])
+# cv_overlap.append(cycle_split(cv_list[37])[2])
+# cv_overlap.append(cycle_split(cv_list[42])[2])
+
+# overlap(cv_overlap,
+        # filename = 'metal_cv',
+        # title = 'Different WE in TMAOH/EmimTFSI, r-Cu, c-C, 10mv/s',
+        # labels = ['GC', 'Ag2O/GC', 'Pb', 'Sn', 'Cd', 'Cu', 'Zn'],
+        # pos = pos)
+
+# plt.close()
 
 pos = plt.subplots(1,1)[1]
 cv_overlap = []
-cv_overlap.append(cv_list[18])
-cv_overlap.append(cv_list[33])
-cv_overlap.append(cv_list[44])
-overlap(cv_overlap, filename = 'test_cv', labels = [1,2,3], pos = pos)
+cv_overlap.append(cycle_split(cv_list[4])[2])
+cv_overlap.append(cycle_split(cv_list[16])[1])
+cv_overlap.append(cycle_split(cv_list[22])[1])
+cv_overlap.append(cycle_split(cv_list[37])[2])
+cv_overlap.append(cycle_split(cv_list[41])[3])
+
+overlap(cv_overlap,
+        filename = 'metal_weird_cv',
+        title = 'Different WE in TMAOH/EmimTFSI, r-Cu, c-C, 10mv/s',
+        labels = ['GC', 'Pb', 'Sn', 'Cu', 'Zn'],
+        pos = pos)
 
 plt.close()
 
-pos = plt.subplots(1,1)[1]
-cv_overlap = []
-cv_overlap.append(cv_list[16])
-cv_overlap.append(cv_list[17])
-cv_overlap.append(cv_list[18])
-overlap(cv_overlap, filename = 'test1_cv', labels = [1,2,3], pos = pos)
+# pos = plt.subplots(1,1)[1]
+# cv_overlap = []
+# cv_overlap.append(cv_list[16])
+# cv_overlap.append(cv_list[17])
+# cv_overlap.append(cv_list[18])
+# overlap(cv_overlap, filename = 'test1_cv', labels = [1,2,3], pos = pos)
 
-pos = plt.subplots(1,1)[1]
-cv_overlap = []
-cv_overlap.append(cv_list[1])
-cv_overlap.append(cv_list[2])
-overlap(cv_overlap, filename = 'test1_cv', labels = [1,2], pos = pos)
+# pos = plt.subplots(1,1)[1]
+# cv_overlap = []
+# cv_overlap.append(cv_list[1])
+# cv_overlap.append(cv_list[2])
+# overlap(cv_overlap, filename = 'test1_cv', labels = [1,2], pos = pos)

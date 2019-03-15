83d553114e1964feddae997ef6dc046a0e5afb07
wsyxbcl <yx_chai@whu.edu.cn>
2019-03-15 23:09:46 +0800

    pathlib.Path & style improved & list_cv to cv_list

--- cv.py
+++ cv.py
@@ -1,9 +1,12 @@
-import matplotlib.pyplot as plt
-from pandas import DataFrame
-from dir_walker import walker
 import re
 import os
 import pickle
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
+from dir_walker import walker
 
 
 class CV:
@@ -29,16 +32,16 @@ class CV:
         pos.set_xlabel('Potential')
         pos.set_ylabel('Current')
 
-    def save(self, filename, path=os.getcwd()):
+    def save(self, filename, path=Path(os.getcwd())):
         header = ['Potential', 'Current']
-        self.data.to_csv(path + '/' + filename + '.csv',
+        self.data.to_csv(path.joinpath(filename+'.csv'),
                          index=False,
                          header=False,
                          columns=header)
-        print("Saved to " + path + filename + '.csv')
+        print("Saved to "+str(path.joinpath(filename+'.csv')))
 
     def __str__(self):
-        return self.info + ' ' + str(self.low) + ' to ' + str(self.high) + ' seg' + str(self.segments)
+        return self.info+' '+str(self.low)+' to '+str(self.high)
 
 
 def find_segments(data):
@@ -64,7 +67,7 @@ def txt_to_CV(file, info='CV test'):
     record = 0
     a = []
     with open(file, 'rt') as f:
-        print(file + ' Opened')
+        print(str(file)+' Opened')
         for line in f:
             if not (h and l):
                 if line.startswith('High'):
@@ -84,7 +87,6 @@ def txt_to_CV(file, info='CV test'):
         potential.append(float(i.split()[0]))
     data = {"Potential": potential, "Current": current}
     frame = DataFrame(data)
-    # cv_info = filename[:len(filename) - 4] # *.txt
     [num_seg, seg_label] = find_segments(frame)
     frame.index = seg_label
     return CV(info, frame, low, high, list(range(1, num_seg + 1)))
@@ -113,7 +115,7 @@ def cycle_split(cv):
     return cv_cycles
 
 
-def overlap(cv_list, *, filename='cv_overlap', title='No title', pos=plt.subplots(1, 1)[1], labels=None):
+def overlap(cv_list, *, path='.', filename='cv_overlap', title='No title', pos=plt.subplots(1, 1)[1], labels=None):
     '''
     Overlap CV_curve in given list
     '''
@@ -139,33 +141,34 @@ def overlap(cv_list, *, filename='cv_overlap', title='No title', pos=plt.subplot
         cv.plot(labels=labels[i], title=title, xlim=[low, high], pos=pos)
     pos.set_xlabel('Potential')
     pos.set_ylabel('Current')
-    plt.savefig(filename + '.png', dpi=300)
+    plt.savefig(path.joinpath(filename), dpi=300, bbox_inches='tight')
 
 
 def cv_collect(rootdir):
     cv_list = []
     for filename, subdir in walker(rootdir, re.compile('cv(.*?)txt')):
-        cv_list.append(txt_to_CV(subdir + '/' + filename, filename[:len(filename) - 4]))
-    with open(rootdir+'cv_list.txt', 'w') as f:
+        cv_list.append(txt_to_CV(Path(subdir).joinpath(filename), filename[:len(filename) - 4]))
+    with open(rootdir.joinpath('list_cv.txt'), 'w') as f:
+        f.truncate()
         for i, cv in enumerate(cv_list):
             f.write(str(i)+' '+cv.info)
             f.write('\n')
     print()
-    print('List of CV has been saved to '+rootdir+'cv_list.txt')
-    with open(rootdir+'cv_data.pickle', 'wb') as f:
+    print('List of CV has been saved to '+str(rootdir)+'cv_list.txt')
+    with open(rootdir.joinpath('data_cv.pickle'), 'wb') as f:
         pickle.dump(cv_list, f, pickle.HIGHEST_PROTOCOL)
     print()
-    print("CV information has been dumped to "+rootdir+'cv_data.pickle')
+    print("CV information has been dumped to "+str(rootdir)+'cv_data.pickle')
     return cv_list
 
 __all__ = ['CV', 'find_segments', 'cycle_split', 'txt_to_CV', 'cv_collect', 'overlap']
 
 if __name__ == '__main__':
-    rootdir = 'T:/programs/python/py_CV/data/'
+    rootdir = Path('C:/code/pyCV/demo')
     for filename, subdir in walker(rootdir, re.compile('cv(.*?)txt')):
         fig, pos = plt.subplots(1, 1)
-        cv = txt_to_CV(subdir + '/' + filename, filename[:len(filename) - 4])
+        cv = txt_to_CV(Path(subdir).joinpath(Path(filename)), info=filename[:-4])
         for i, cv_cycle in enumerate(cycle_split(cv)):
             cv_cycle.plot(labels='cycle ' + str(i + 1), pos=pos)
-        plt.savefig(subdir + '/' + str(cv) + '.png', dpi=300)
-        print(subdir + '/' + str(cv) + '.png' + ' Saved')
+        plt.savefig(Path(subdir).joinpath(str(cv)+'.png'), dpi=300, bbox_inches='tight') # matploblib now has PathLike support
+        print(str(Path(subdir).joinpath(str(cv)+'.png'))+' Saved')
--- overlap.py
+++ overlap.py
@@ -1,34 +1,19 @@
-from cv import *
-rootdir = 'T:/programs/python/py_CV/data/'
-cv_list = cv_collect(rootdir)
 import os.path
 import pickle
+from pathlib import Path
 
-rootdir = 'T:/programs/python/py_CV/test/'
+import matplotlib.pyplot as plt
 
-if os.path.isfile(rootdir+'cv_data.pickle'):
-    with open(rootdir+'cv_data.pickle', 'rb') as f:
-        cv_list = pickle.load(f)
-else:
-    cv_list = cv_collect(rootdir)
+from cv import *
 
-# pos = plt.subplots(1,1)[1]
-# cv_overlap = []
-# cv_overlap.append(cycle_split(cv_list[4])[2])
-# cv_overlap.append(cycle_split(cv_list[11])[2])
-# cv_overlap.append(cycle_split(cv_list[18])[2])
-# cv_overlap.append(cycle_split(cv_list[23])[8])
-# cv_overlap.append(cycle_split(cv_list[33])[2])
-# cv_overlap.append(cycle_split(cv_list[37])[2])
-# cv_overlap.append(cycle_split(cv_list[42])[2])
+rootdir = Path('C:/code/pyCV/demo/overlap')
+cv_list = cv_collect(rootdir)
 
-# overlap(cv_overlap,
-        # filename = 'metal_cv',
-        # title = 'Different WE in TMAOH/EmimTFSI, r-Cu, c-C, 10mv/s',
-        # labels = ['GC', 'Ag2O/GC', 'Pb', 'Sn', 'Cd', 'Cu', 'Zn'],
-        # pos = pos)
-
-# plt.close()
+# if os.path.isfile(rootdir.joinpath('cv_data.pickle')):
+with open(rootdir.joinpath('data_cv.pickle'), 'rb') as f:
+    cv_list = pickle.load(f)
+# else:
+# cv_list = cv_collect(rootdir)
 
 pos = plt.subplots(1,1)[1]
 cv_overlap = []
@@ -39,22 +24,10 @@ cv_overlap.append(cycle_split(cv_list[37])[2])
 cv_overlap.append(cycle_split(cv_list[41])[3])
 
 overlap(cv_overlap,
-        filename = 'metal_weird_cv',
+        path=rootdir,
+        filename = 'metal_weird_cv.png',
         title = 'Different WE in TMAOH/EmimTFSI, r-Cu, c-C, 10mv/s',
         labels = ['GC', 'Pb', 'Sn', 'Cu', 'Zn'],
         pos = pos)
 
 plt.close()
-
-# pos = plt.subplots(1,1)[1]
-# cv_overlap = []
-# cv_overlap.append(cv_list[16])
-# cv_overlap.append(cv_list[17])
-# cv_overlap.append(cv_list[18])
-# overlap(cv_overlap, filename = 'test1_cv', labels = [1,2,3], pos = pos)
-
-# pos = plt.subplots(1,1)[1]
-# cv_overlap = []
-# cv_overlap.append(cv_list[1])
-# cv_overlap.append(cv_list[2])
-# overlap(cv_overlap, filename = 'test1_cv', labels = [1,2], pos = pos)
